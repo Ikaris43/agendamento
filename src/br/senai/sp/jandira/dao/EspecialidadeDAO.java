3dd0d976a697e628576b7410da28c17214454352
Ikaris <log4304.016@gmail.com>
2022-11-17 16:57:39 -0300

    terminando especialidade

--- src/br/senai/sp/jandira/dao/EspecialidadeDAO.java
+++ src/br/senai/sp/jandira/dao/EspecialidadeDAO.java
@@ -1,35 +1,38 @@
-
 package br.senai.sp.jandira.dao;
 
 import br.senai.sp.jandira.model.Especialidade;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-
 public class EspecialidadeDAO {
-    
+
     private final static String URL = "C:\\Users\\22282204\\Java\\especialidade.txt";
+    private final static String URL_TEMP = "C:\\Users\\22282204\\Java\\especialidade-temp.txt";
     private final static Path PATH = Paths.get(URL);
-    
+    private final static Path PATH_TEMP = Paths.get(URL_TEMP);
+
     private static ArrayList<Especialidade> especialidades = new ArrayList<>();
 
     public static ArrayList<Especialidade> getEspecialidades() {
         return especialidades;
     }
-    
+
     public static DefaultTableModel getEspecialidadesModel() {
-        
+
         String[] titulos = {"Código", "Nome da Especialidade", "Descrição"};
         String[][] dados = new String[especialidades.size()][3];
-        
+
         int i = 0;
         for (Especialidade e : especialidades) {
             dados[i][0] = e.getCodigo().toString();
@@ -40,90 +43,123 @@ public class EspecialidadeDAO {
         DefaultTableModel model = new DefaultTableModel(dados, titulos);
         return model;
     }
-    
-    
+
     public static Especialidade getEspecialidade(Integer codigo) {
-        for(Especialidade e : especialidades) {
+        for (Especialidade e : especialidades) {
             if (codigo == e.getCodigo()) {
                 return e;
             }
         }
         return null;
     }
-    
+
     public static void excluir(Integer codigo) {
-        for(Especialidade e : especialidades ) {
-         if (codigo == e.getCodigo()) {
-            especialidades.remove(e);
-            break;
-         }
-            
-    }
-    }
-    
-    public static void atualizar (Especialidade correta){
-        for (Especialidade e : especialidades ) {
-            if (correta.getCodigo() == e.getCodigo()){
-                int posicao = especialidades.indexOf(e);
-                especialidades.set(posicao, correta);
-                
+
+        for (Especialidade e : especialidades) {
+            if (e.getCodigo().equals(codigo)) {
+                especialidades.remove(e);
+                break;
             }
         }
+        atualizarArquivo();
     }
-    
-    public static void gravar(Especialidade e) {
-        especialidades.add(e);
-        
-        //Gravar em arquivo
-        //Decompor um Objeto em String - Serialização
-        
+
+    private static void atualizarArquivo() {
+        //PASSO 1 - Criar uma representação dos arquivos que serão manipulados
+        File arquivoAtual = new File(URL);
+        File arquivoTemp = new File(URL_TEMP);
+
         try {
-            BufferedWriter escritor = Files.newBufferedWriter( PATH,
+            // Criar o arquivo temporário
+            arquivoTemp.createNewFile();
+
+            // Abrir o arquivo temporário para escrita 
+            BufferedWriter bwTemp = Files.newBufferedWriter(
+                    PATH_TEMP,
                     StandardOpenOption.APPEND,
                     StandardOpenOption.WRITE);
+
+            // Iterar na lista para adicionar as especialidades
+            // no arquivo temporário, exceto o registro que 
+            // não queremos mais
             
+            for (Especialidade e : especialidades) {
+                bwTemp.write(e.getEspecialidadeSeparadaPorPontoEVirgula());
+                bwTemp.newLine();
+            }
+            
+            bwTemp.close();
+            
+            arquivoAtual.delete();
+            arquivoTemp.renameTo(arquivoAtual);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void atualizar(Especialidade correta) {
+        for (Especialidade e : especialidades) {
+            if (correta.getCodigo() == e.getCodigo()) {
+                int posicao = especialidades.indexOf(e);
+                especialidades.set(posicao, correta);
+
+            }
+        }
+        atualizarArquivo();
+    }
+
+    public static void gravar(Especialidade e) {
+        especialidades.add(e);
+
+        //Gravar em arquivo
+        //Decompor um Objeto em String - Serialização
+        try {
+            BufferedWriter escritor = Files.newBufferedWriter(PATH,
+                    StandardOpenOption.APPEND,
+                    StandardOpenOption.WRITE);
+
             escritor.write(e.getEspecialidadeSeparadaPorPontoEVirgula());
             escritor.newLine();
             escritor.close();
         } catch (IOException error) {
             JOptionPane.showMessageDialog(null, "Ocorreu um erro");
         }
-        
+
     }
-    
+
     //Criar uma lista inicial de especialidades
-    public static void criarListaDeEspecialidaddes(){
-        
+    public static void criarListaDeEspecialidaddes() {
+
         try {
             BufferedReader leitor = Files.newBufferedReader(PATH);
-            
+
             String linha = leitor.readLine();
-            
+
             while (linha != null) {
                 //Transformar os dados da linha em uma especialidade
-                
+
                 String[] vetor = linha.split(";");
-                
-                
+
                 Especialidade e = new Especialidade(
                         vetor[1],
                         vetor[2],
                         Integer.valueOf(vetor[0]));
-                
+
                 //Guardar a especialidade na lista
                 especialidades.add(e);
-                
+
                 //Ler a próxima linha
                 linha = leitor.readLine();
+
                 
-                leitor.close();
             }
-            
+            leitor.close();
+
         } catch (IOException erro) {
             JOptionPane.showMessageDialog(null, "Erro 2");
-       }
-        
-        
+        }
+
     }
-    
+
 }
--- src/br/senai/sp/jandira/dao/PlanoDeSaudeDAO.java
+++ src/br/senai/sp/jandira/dao/PlanoDeSaudeDAO.java
@@ -1,5 +1,6 @@
 package br.senai.sp.jandira.dao;
 
+import br.senai.sp.jandira.model.Especialidade;
 import br.senai.sp.jandira.model.PlanoDeSaude;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -15,23 +16,25 @@ import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.DateFormatter;
 
-
 public class PlanoDeSaudeDAO {
-    
+
     private static final String URL = "C:\\Users\\22282204\\Java\\planoDeSaude.txt";
     private static final Path PATH = Paths.get(URL);
-    
+
     private static ArrayList<PlanoDeSaude> planosDeSaude = new ArrayList<>();
     
-    public static ArrayList<PlanoDeSaude> getPlanosDeSaude () {
+
+    
+
+    public static ArrayList<PlanoDeSaude> getPlanosDeSaude() {
         return planosDeSaude;
     }
-    
-    public static DefaultTableModel getPlanosDeSaudeModel () {
-        
+
+    public static DefaultTableModel getPlanosDeSaudeModel() {
+
         String[] titulos = {"Código", "Operadora", "Número", "Categoria", "Validade"};
         String[][] dados = new String[planosDeSaude.size()][5];
-        
+
         int i = 0;
         for (PlanoDeSaude e : planosDeSaude) {
             dados[i][0] = e.getCodigo().toString();
@@ -41,137 +44,97 @@ public class PlanoDeSaudeDAO {
             DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
             dados[i][4] = e.getValidade().format(formato);
             i++;
-            
+
         }
         DefaultTableModel model = new DefaultTableModel(dados, titulos);
         return model;
     }
-    
+
     public static PlanoDeSaude getPlanoDeSaude(Integer plano) {
-        for(PlanoDeSaude e : planosDeSaude) {
+        for (PlanoDeSaude e : planosDeSaude) {
             if (plano == e.getCodigo()) {
                 return e;
             }
         }
         return null;
     }
-    
-       public static void excluir(Integer codigo) {
-        for(PlanoDeSaude e : planosDeSaude ) {
-         if (codigo == e.getCodigo()) {
-            planosDeSaude.remove(e);
-            break;
-         }
-            
+
+    public static void excluir(Integer codigo) {
+        for (PlanoDeSaude e : planosDeSaude) {
+            if (codigo == e.getCodigo()) {
+                planosDeSaude.remove(e);
+                break;
+            }
+
+        }
     }
-    }
-       
-       public static void gravar(PlanoDeSaude e) {
+
+    public static void gravar(PlanoDeSaude e) {
         planosDeSaude.add(e);
-        
+
         try {
             BufferedWriter escritor = Files.newBufferedWriter(PATH, StandardOpenOption.APPEND, StandardOpenOption.WRITE);
-            
+
             escritor.write(e.getPlanoDeSaudeFormatadoComPontoEVirgula());
             escritor.newLine();
             escritor.close();
-            
+
         } catch (IOException error) {
             JOptionPane.showMessageDialog(null, "Erro!");
         }
     }
-       
-    public static void atualizar (PlanoDeSaude correta){
-        for (PlanoDeSaude e : planosDeSaude ) {
-            if (correta.getCodigo() == e.getCodigo()){
+
+    public static void atualizar(PlanoDeSaude correta) {
+        for (PlanoDeSaude e : planosDeSaude) {
+            if (correta.getCodigo() == e.getCodigo()) {
                 int posicao = planosDeSaude.indexOf(e);
                 planosDeSaude.set(posicao, correta);
-                
+
             }
         }
     }
-    
-    public static void criarListaDePlanosDeSaude(){
-        
+
+    public static void criarListaDePlanosDeSaude() {
+
         try {
             BufferedReader leitor = Files.newBufferedReader(PATH);
-            
+
             String linha = leitor.readLine();
-            
+
             while (linha != null) {
-                
-             
-                
+
                 String[] vetor = linha.split(";");
-                
+
                 String[] dataCortada = vetor[4].split("-");
                 String dia = dataCortada[2];
                 String mes = dataCortada[1];
                 String ano = dataCortada[0];
+
+                String dataFormatada = dia + "/" + mes + "/" + ano;
+
                 
-                String dataFormatada = dia + mes + ano;
-                
-                DateTimeFormatter formato = DateTimeFormatter.ofPattern(dataFormatada);
-                
-              
-                
-                
-                
+
                 PlanoDeSaude e = new PlanoDeSaude(
                         vetor[1],
                         vetor[2],
                         vetor[3],
-                        LocalDate.parse(dataFormatada),
+                        LocalDate.parse(vetor[4]),
                         Integer.valueOf(vetor[0]));
-                
-                
-               
-           
+
                 planosDeSaude.add(e);
-                
+
                 linha = leitor.readLine();
+
                 
-                leitor.close();
             }
-            
+            leitor.close();
+
         } catch (IOException erro) {
             JOptionPane.showMessageDialog(null, "Erro 3!");
         }
-        
+
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     
     
 }
